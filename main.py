bf1b63c8ad5c68fe5062a20f36c1822ad06edc9a
joelvogt <bullfishfighter@gmail.com>
2015-04-01 20:17:20 +0200

    Refreshing table with time periods

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ manager = Manager(app)
 # Turn on debugger by default and reloader
 manager.add_command("runserver", Server(
     use_debugger=False,
-    use_reloader=True,
-    host='0.0.0.0',
-    port=8080)
+    use_reloader=True)
 )
 
 if __name__ == "__main__":
